1e2a7ad7ddf47cb08ff546e1f85b68af4d8572a1
Adeline Ackun <adelineackun@gmail.com>
2024-06-10 09:48:59 +0000

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn-btn')
+btn.onclick = function(){
+    alert('Contact Loading....')
+}
+
+const ade = document.getElementById('more-btn')
+ade.onclick = function(){
+    alert('More')
+}
